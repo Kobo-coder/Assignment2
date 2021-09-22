25feded8996afe23647cfd034248c01c561eb1c8
AntonBertelsen <anton.bertelsen@gmail.com>
2021-09-22 13:28:02 +0200

    Finish exercise 1

--- Student.Tests/UnitTest1.cs
+++ Student.Tests/UnitTest1.cs
@@ -25,14 +25,95 @@ namespace Student.Tests
         public void given_student_Surname_Jakobsen_return_Jakobsen()
         {
         
-        var testStudent = new Student(2);
-        testStudent.Surname = "Jakobsen";
+            var testStudent = new Student(2);
+            testStudent.Surname = "Jakobsen";
 
-        Assert.Equal("Jakobsen", testStudent.Surname);
+            Assert.Equal("Jakobsen", testStudent.Surname);
         
         }
 
+        [Fact]
+        public void given_student_Startdate_2021_1_1_return_Startdate_2021_1_1()
+        {
+            var testStudent = new Student(3);
+            testStudent.StartDate = new DateTime(2021,1,1);
+            
+            Assert.Equal(new DateTime(2021,1,1),testStudent.StartDate);
+        }
         
+        [Fact]
+        public void given_student_Enddate_2021_1_2_return_Enddate_2021_1_2()
+        {
+            var testStudent = new Student(4);
+            testStudent.EndDate = new DateTime(2021,1,2);
+            
+            Assert.Equal(new DateTime(2021,1,2),testStudent.EndDate);
+        }
 
+        [Fact]
+        public void given_student_Graduationdate_2021_1_3_return_Graduationdate_2021_1_3()
+        {
+            var testStudent = new Student(5);
+            testStudent.GraduationDate = new DateTime(2021,1,4);
+            
+            Assert.Equal(new DateTime(2021,1,4),testStudent.GraduationDate);
+        }
+
+        [Fact]
+        public void given_student_StartDate_2021_1_1_EndDate_2023_1_1_GraduationDate_23_1_1_return_status_new()
+        {
+            var testStudent = new Student(6);
+            testStudent.StartDate = new DateTime(2021,1,1);
+            testStudent.EndDate = new DateTime(2023,1,1);
+            testStudent.GraduationDate = new DateTime(2023,1,1);
+            
+            Assert.Equal(Status.New,testStudent.Status);
+        }
+
+        [Fact]
+        public void given_student_StartDate_2015_1_1_EndDate_2023_1_1_GraduationDate_23_1_1_return_status_active()
+        {
+            var testStudent = new Student(7);
+            testStudent.StartDate = new DateTime(2015,1,1);
+            testStudent.EndDate = new DateTime(2023,1,1);
+            testStudent.GraduationDate = new DateTime(2023,1,1);
+            
+            Assert.Equal(Status.Active,testStudent.Status);
+        }
+        
+        [Fact]
+        public void given_student_StartDate_2015_1_1_EndDate_2020_1_1_GraduationDate_2020_1_1_return_status_graduated()
+        {
+            var testStudent = new Student(8);
+            testStudent.StartDate = new DateTime(2015,1,1);
+            testStudent.EndDate = new DateTime(2020,1,1);
+            testStudent.GraduationDate = new DateTime(2020,1,1);
+            
+            Assert.Equal(Status.Graduated,testStudent.Status);
+        }
+
+        [Fact]
+        public void given_student_StartDate_2015_1_1_EndDate_2019_1_1_GraduationDate_2020_1_1_return_status_dropout()
+        {
+            var testStudent = new Student(9);
+            testStudent.StartDate = new DateTime(2015,1,1);
+            testStudent.EndDate = new DateTime(2019,1,1);
+            testStudent.GraduationDate = new DateTime(2020,1,1);
+            
+            Assert.Equal(Status.Dropout,testStudent.Status);
+        }
+        
+        [Fact]
+        public void given_student_StartDate_2015_1_1_EndDate_2019_1_1_GraduationDate_2020_1_1_Name_Bob_Surname_Bobson_return_FancyString()
+        {
+            var testStudent = new Student(10);
+            testStudent.StartDate = new DateTime(2015,1,1);
+            testStudent.EndDate = new DateTime(2019,1,1);
+            testStudent.GraduationDate = new DateTime(2020,1,1);
+            testStudent.GivenName = "Bob";
+            testStudent.Surname = "Bobson";
+            
+            Assert.Equal("ID: 10, Name: Bob Bobson, StartDate: 01.01.2015, EndDate: 01.01.2019, GraduationDate: 01.01.2020, Status: Dropout",testStudent.ToString());
+        }
     }
 }
--- Student/Class1.cs
+++ Student/Class1.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 
+
 namespace Student
 {
     public class Student
@@ -7,12 +8,38 @@ namespace Student
         public int id { get; }
 
         public string GivenName { get; set; }
-        
-        public string Surname { get; set; }
-        
 
+        public string Surname { get; set; }
+
+        public DateTime StartDate { get; set; }
+    
+        public DateTime EndDate { get; set; }
+
+        public DateTime GraduationDate { get; set; }
+
+        public Status Status { get { 
+            if(EndDate < GraduationDate){
+                return Status.Dropout;
+            }
+            else if(GraduationDate > DateTime.Now && EndDate > DateTime.Now ){
+                if(DateTime.Now.Subtract(StartDate) < new TimeSpan(365,0,0,0)){
+                    return Status.New;
+                }
+                else{
+                    return Status.Active;
+                }
+            }
+            //else if (GraduationDate <= DateTime.Now && GraduationDate == EndDate){
+                return Status.Graduated;
+            //} 
+        }}
+        
         public Student(int id){
             this.id = id;
+        }
+
+        public string ToString(){
+            return "ID: " + id + ", Name: " + GivenName + " " + Surname + ", StartDate: " + StartDate.ToString("MM/dd/yyyy") + ", EndDate: " + EndDate.ToString("MM/dd/yyyy") + ", GraduationDate: " + GraduationDate.ToString("MM/dd/yyyy") + ", Status: " + Status;
         }
     }
 }
